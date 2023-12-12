9d10d6466785a52d53230889c2c6f81b0d5c37d2
Sven Welink <78083196+svenwelink@users.noreply.github.com>
2023-12-12 18:36:08 +0100

    Create day02.py

---
+++ day02.py
@@ -0,0 +1,81 @@
+# Day 2
+# Import data
+def importData(path):
+    file = open(pathFile,'r')
+    data = file.readlines()
+    file.close()
+    return(data)
+
+pathFile = #Data path
+data = importData(pathFile)
+
+resetValue, nextValue = ";", ","
+
+def runGame(line):
+    gameId, game = line[5:].split(":")
+    gameParts = game.split(resetValue)
+
+    for i in range (0, len(gameParts)):
+        grabs = gameParts[i].split(nextValue)
+
+        for j in range(0, len(grabs)):
+            grab = grabs[j].split(" ")
+            
+
+            if grab[2][0:3] == "red" and int(grab[1]) > 12:
+                return(0)
+            elif grab[2][0:5] == "green" and int(grab[1]) > 13:
+                return(0)
+            elif grab[2][0:4] == "blue" and int(grab[1]) > 14:
+                return(0)
+
+    return(int(gameId))
+
+def runPartOne():
+    totalSum = 0
+    for lineId in range(0, len(data)):
+        score = runGame(data[lineId])
+        if score > 0:
+            totalSum += score
+
+    return(totalSum)
+
+print("Day 2")
+print("Part 1")
+resultPartOne = runPartOne()
+print(resultPartOne)
+
+# Part 2
+def runGamePartTwo(line):
+    gameId, game = line[5:].split(":")
+    gameParts = game.split(resetValue)
+    minRed, minGreen, minBlue = 0, 0, 0
+
+    for i in range (0, len(gameParts)):
+        grabs = gameParts[i].split(nextValue)
+
+        for j in range(0, len(grabs)):
+            grab = grabs[j].split(" ")
+        
+            if grab[2][0:3] == "red":
+                minRed = max(minRed, int(grab[1]))
+            elif grab[2][0:5] == "green":
+                minGreen = max(minGreen, int(grab[1]))
+            elif grab[2][0:4] == "blue":
+                minBlue = max(minBlue, int(grab[1]))
+
+    product = minRed * minGreen * minBlue
+    return(int(product))
+
+def runPartTwo():
+    totalSum = 0
+    for lineId in range(0, len(data)):
+        score = runGamePartTwo(data[lineId])
+        totalSum += score
+
+    return(totalSum)
+
+print("Day 2")
+print("Part 2")
+resultPartTwo = runPartTwo()
+print(resultPartTwo)
