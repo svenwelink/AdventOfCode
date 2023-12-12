01c8d01c1bd5da6a0b63139cadf90f6c2dd7f8c9
Sven Welink <78083196+svenwelink@users.noreply.github.com>
2023-12-12 18:40:58 +0100

    Create day06.py

---
+++ 2023/day06.py
@@ -0,0 +1,84 @@
+# Day 6
+print("Day 6")
+
+def importData(path):
+    file = open(pathFile,'r')
+    data = file.readlines()
+    file.close()
+    return(data)
+
+pathFile = "C:/Users/svenw/Documents/ProgrammeerProjecten/Advent/2023/day06/input.txt"
+data = importData(pathFile)
+
+def removeSpacesList(list):
+    list = ' '.join(list).split()
+    return(list)
+
+def changeStringListToInterger(numberList):
+    numberList = list(map(int, numberList))
+    return(numberList)
+
+def getListFromRow(row):
+    header, data = row.split(":")
+    dataList = data.split(" ")
+    dataList = removeSpacesList(dataList)
+    dataList = changeStringListToInterger(dataList)
+    return(dataList) 
+
+def getProductFromList(listProduct):
+    product = 1
+    for i in listProduct:
+        product = product * i
+    return(product) 
+
+def runPartOne():
+    timeList, distanceList = getListFromRow(data[0]), getListFromRow(data[1])
+    newRecordList = []
+
+    for i in range(len(timeList)):
+        maxTime = int(timeList[i])
+        recordCount = 0
+
+        for xSeconds in range(maxTime):
+            speed, timeToRace = int(xSeconds), (maxTime - int(xSeconds))
+            distance = speed * timeToRace
+            if distance > distanceList[i]:
+                recordCount += 1
+
+        newRecordList.append(int(recordCount))
+
+    # After all racing is done calculate score from newRecordList
+    return(getProductFromList(newRecordList))
+
+print("part 1")
+resultPartOne = runPartOne()
+print(resultPartOne)
+
+# Part 2
+def getValueFromRow(row):
+    header, data = row.split(":")
+    data = int(data.replace(" ", ""))
+    return(data) 
+
+def getFirstTimeBeatDistance(time, distance):
+    for speed in range(time):
+        remainingTime = time - speed
+        totalDistance = speed * remainingTime
+        if totalDistance > distance:
+            return(speed)
+        
+    print("No speed found to beat record")
+    return(False)
+
+def runPartTwo():
+    timeSlots, distanceToBeat = getValueFromRow(data[0]), getValueFromRow(data[1])
+
+    firstTimeBeat = getFirstTimeBeatDistance(timeSlots, distanceToBeat)
+    lastTimeToBeat = timeSlots - firstTimeBeat
+    
+    totalBeatTimes = lastTimeToBeat - firstTimeBeat + 1
+    return(totalBeatTimes)
+
+print("part 2")
+resultTwo = runPartTwo()
+print(resultTwo)
