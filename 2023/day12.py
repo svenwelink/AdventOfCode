430904216748b768578114a7ef021b835db2617e
Sven Welink <78083196+svenwelink@users.noreply.github.com>
2023-12-13 22:13:33 +0100

    Create day12.py
    Need to be finished

---
+++ 2023/day12.py
@@ -0,0 +1,83 @@
+# Day 12
+print("Day 12")
+
+def importData(path):
+    file = open(path,'r')
+    data = file.readlines()
+    file.close()
+    return(data)
+
+pathFileData = # Path
+pathFileTestData = # Path
+data = importData(pathFileData)
+test = importData(pathFileTestData)
+
+def changeStringListToInterger(numberList):
+    numberList = list(map(int, numberList))
+    return(numberList)
+
+def formatData(dataRow):
+    row = dataRow.strip()
+    field, numbers = row.split(" ")
+    numbersListString = numbers.split(",")
+    numbersList = changeStringListToInterger(numbersListString)
+    return(field, numbersList)
+
+def runPartOne(data):
+    totalPossibilities = 0
+
+    for i in range(len(data)):
+        fieldString, numbers = formatData(data[i])
+        allFieldStrings = getAllPosibleFieldStrings(fieldString)
+
+        for string in allFieldStrings:
+            if string.count("#") == sum(numbers): # Do not check de next line before beceaus it takes more time
+                if getNumberListFromFieldString(string) == numbers:
+                    totalPossibilities += 1
+
+    return(totalPossibilities)
+
+def getNumberListFromFieldString(fieldString):
+    numberList = []
+    number = 0
+    for i in range(len(fieldString)):
+        if fieldString[i] == "#":
+            number += 1
+        elif number > 0:
+            numberList.append(number)
+            number = 0
+    if number > 0:
+        numberList.append(number)
+    return(numberList)
+
+def getAllPosibleFieldStrings(fieldString):
+    fieldStrings = []
+
+def getAllPosibleFieldStrings(fieldString):
+    fieldStringAsList = list(fieldString)
+    fieldStrings = []
+    countQuestionMark = fieldStringAsList.count("?")
+
+    for x in range(2 ** countQuestionMark):
+        copyString = fieldStringAsList
+        binaryNumberString = str(bin(x))[2:]
+        binaryNumberString = binaryNumberString.zfill(countQuestionMark)
+        print(binaryNumberString[0])
+        countQuestionMarksReplaced = 0
+        for i in range(len(copyString)):
+            if copyString[i] == "?":
+                if binaryNumberString[countQuestionMarksReplaced] == "0":
+                    copyString[i] = "."
+                    print("TESTS")
+                else:
+                    copyString[i] = "#"
+                    print("TESTS")
+                countQuestionMarksReplaced = countQuestionMarksReplaced + 1
+            
+        fieldStrings.append(copyString)
+
+    return(fieldStrings)
+
+getAllPosibleFieldStrings("??#..#")
+
+# Still needs some work
