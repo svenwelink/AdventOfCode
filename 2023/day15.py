663105bb6d0e2200e9d8f91fe0690b7105d6cc2c
Sven Welink <78083196+svenwelink@users.noreply.github.com>
2023-12-15 20:15:25 +0100

    Create day15.py
    Both stars, final part 2 could be split into multiple functions

---
+++ 2023/day15.py
@@ -0,0 +1,92 @@
+# Day 15
+print("Day 15")
+
+# Import data
+def importData(path):
+    file = open(path,'r')
+    data = file.readlines()
+    file.close()
+    return(data)
+
+pathFileData = # Path
+pathFileTestData = # Path
+data = importData(pathFileData)
+test = importData(pathFileTestData)
+
+def makeList(data):
+    dataList = data[0].split(",")
+    return(dataList)
+
+def calculateNextValue(currentValue, character):
+    valueChar = ord(character)
+    currentValue += valueChar
+    currentValue = currentValue * 17
+    currentValue = currentValue % 256
+    return(currentValue)
+
+def runPartOne(data):
+    totalSum = 0
+    data = makeList(data)
+    for string in data:
+        currentValue = 0
+        for charId in range(len(string)):
+            currentValue = calculateNextValue(currentValue, string[charId])
+        totalSum += currentValue
+
+    return(totalSum)
+
+print("Part 1")
+print("Test:", runPartOne(test))
+print("Result:", runPartOne(data))
+
+def makeEmptyList(length = 256):
+    emptyList = []
+    for i in range(length):
+        emptyList.append([])
+    return(emptyList)
+
+def getAllBoxesWithLens(data):
+    data = makeList(data)
+
+    boxes, lensNames = makeEmptyList(), makeEmptyList()
+    for string in data:
+        currentValue, boxNumber = 0, -1
+        for charId in range(len(string)):
+            if string[charId] == "-":
+                boxNumber = currentValue
+
+                if string[:charId] in lensNames[boxNumber]:
+                    placeInBox = lensNames[boxNumber].index(string[:charId])
+                    del boxes[boxNumber][placeInBox]
+                    del lensNames[boxNumber][placeInBox]
+
+            elif string[charId] == "=":
+                boxNumber = currentValue
+
+                if string[:charId] in lensNames[boxNumber]:
+                    placeInBox = lensNames[boxNumber].index(string[:charId])
+                    boxes[boxNumber][placeInBox] = int(string[charId + 1])
+                    lensNames[boxNumber][placeInBox] = string[:charId]
+               
+                else:
+                    boxes[boxNumber].append(int(string[charId + 1]))
+                    lensNames[boxNumber].append(string[:charId])
+
+            elif boxNumber == -1:
+                currentValue = calculateNextValue(currentValue, string[charId])
+        
+    return(boxes)
+
+def runPartTwo(data):
+    boxes = getAllBoxesWithLens(data)
+    totalSum = 0
+    for i in range(len(boxes)):
+        for j in range(len(boxes[i])):  
+            valueRow = (j+1) * int(boxes[i][j]) * (i+ 1)
+            totalSum += valueRow
+
+    return(totalSum)
+
+print("Part 2")
+print("Test:", runPartTwo(test))
+print("Result:", runPartTwo(data))
