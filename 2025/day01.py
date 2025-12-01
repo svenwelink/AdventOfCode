6c4806db62c10688d27340ec28bc8cea6b2778ae
Sven Welink <78083196+svenwelink@users.noreply.github.com>
2025-12-01 19:23:27 +0100

    Implement functions for dial operations

---
+++ 2025/day01.py
@@ -0,0 +1,64 @@
+import utils
+
+test = utils.importData("TestInput/day01.txt")
+input = utils.importData("Input/day01.txt")
+
+def splitDirectionAndAmount(stringValue):
+    return(stringValue[0], int(stringValue[1:]))
+
+def runPartOne(data):
+    dialValue, timesZeroPassed = 50, 0
+
+    for instruction in data:
+        direction, amount = splitDirectionAndAmount(instruction)
+
+        # add the amount
+        if direction == "R":
+            dialValue += amount
+        elif direction == "L":
+            dialValue -= amount
+        
+        # check if 0 is passed
+        if dialValue < 0 or dialValue > 99:
+            dialValue = dialValue % 100
+
+        # check if value is zero
+        if dialValue == 0:
+            timesZeroPassed += 1
+    
+    return(timesZeroPassed)
+        
+print(runPartOne(input))
+
+def runPartTwo(data):
+    dialValue, timesZeroPassed = 50, 0
+
+    for instruction in data:
+        direction, amount = splitDirectionAndAmount(instruction)
+
+        # add the amount
+        if direction == "R":
+            dialValue += amount
+        elif direction == "L":
+            dialValue -= amount
+
+         # check if 0 is passed
+        if dialValue == 0:
+            timesZeroPassed += 1
+
+        elif dialValue < 0:
+            # if the value was previusly zero
+            if dialValue + amount == 0: 
+                timesZeroPassed += (dialValue * -1 // 100) 
+                dialValue = dialValue % 100
+            # if extra turns are done
+            else:
+                timesZeroPassed += dialValue * -1 // 100 + 1
+                dialValue = dialValue % 100
+                
+        elif dialValue > 99:
+            timesZeroPassed += dialValue // 100
+            dialValue = dialValue % 100
+
+    return(timesZeroPassed)
+print(runPartTwo(input))
