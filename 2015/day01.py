169141ed9dd82db01632574b611918bcb790d0d1
Sven Welink <78083196+svenwelink@users.noreply.github.com>
2025-12-03 22:12:15 +0100

    Solution
    Implement functions to calculate floor based on input signs.

---
+++ 2015/day01.py
@@ -0,0 +1,23 @@
+import utils
+
+input = utils.importData("Input/day01.txt")[0]
+
+def getTheRightFloor(sign):
+    if sign == ")": 
+        return -1
+    return 1
+
+def runPartOne(data, height=0):
+    for i in range(len(data)):
+        height += getTheRightFloor(data[i])
+    return height
+
+print(runPartOne(input))
+        
+def runPartTwo(data, height=0):
+    for i in range(len(data)):
+        height += getTheRightFloor(data[i])
+        if height == -1: return i + 1
+    return len(data)
+
+print(runPartTwo(input))
