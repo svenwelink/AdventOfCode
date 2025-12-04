b65f8b73afc4dc275a3c10c9f852dd798755f3c2
Sven Welink <78083196+svenwelink@users.noreply.github.com>
2025-12-04 17:27:47 +0100

    Change diagram representation from '0' to '.'

--- 2025/day04.py
+++ 2025/day04.py
@@ -7,22 +7,22 @@ def turnStringIntoList(row):
     return [row[x] for x in range(len(row))]
 
 def prepDiagram(data):
-    df = [x.strip().replace(".", "0").replace("@", "1") for x in data]
+    df = [x.strip() for x in data]
     padddingRow = turnStringIntoList("0" * (len(df[0]) + 2))
     frameWithPadding = []
     frameWithPadding.append(padddingRow)
     for i in range(len(df)):
-        frameWithPadding.append(turnStringIntoList("0" + df[i] + "0"))
+        frameWithPadding.append(turnStringIntoList("." + df[i] + "."))
     frameWithPadding.append(padddingRow)
     
     return frameWithPadding
 
 def isRollAccessible(df, pos):
-    if df[pos[0]][pos[1]] == "0":
+    if df[pos[0]][pos[1]] == ".":
         return False
     
     stringAroundPos = str(df[pos[0]-1][(pos[1]-1):(pos[1]+2)]) + str(df[pos[0]][pos[1]-1]) + str(df[pos[0]][pos[1]+1]) + str(df[pos[0]+1][(pos[1]-1):(pos[1]+2)])
-    if stringAroundPos.count("1") < 4:
+    if stringAroundPos.count("#") < 4:
         return True
 
     return False
@@ -45,7 +45,7 @@ def runPartTwo(data, totalCount = 0, removedLastRound = 1):
         for x in range(1, len(diagram)-1): 
             for y in range(1, len(diagram[x])):
                 if isRollAccessible(diagram, [x, y]):
-                    diagram[x][y] = "0"
+                    diagram[x][y] = "."
                     removedLastRound += 1
 
         totalCount += removedLastRound
