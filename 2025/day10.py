544c6e417b336e037ecd3af974def34380785b76
Sven Welink <78083196+svenwelink@users.noreply.github.com>
2025-12-10 15:47:16 +0100

    Refactor combination handling in day10.py

--- 2025/day10.py
+++ 2025/day10.py
@@ -1,5 +1,5 @@
 import utils
-import itertools
+from itertools import combinations
 
 test = utils.importData("TestInput/day10.txt")
 input = utils.importData("Input/day10.txt")
@@ -38,7 +38,7 @@ def translateInputStringToLPInput(string):
 
 def calculatCombination(combination):
     if len(combination) == 1:
-        return combination
+        return combination[0]
     newCombination = combination[0]
     for button in combination[1:]:
         for id in range(len(combination)):
@@ -49,22 +49,24 @@ def combinationIsGood(solution, combination):
     for id in range(len(solution)):
         if combination[id] % 2 != solution[id]:
             return False
+    print(combination)
     return True
 
-def getAllCombinations(buttons):
-    buttonCombinations = []
-    for i in range(1, len(buttons) + 1):
-        buttonCombinations.append(list(itertools.combinations([x for x in range(len(buttons))], i)))
-    print(buttonCombinations)
-    return list(itertools.combinations(buttons, len(buttons)))
+def getCombinationIds(buttonList):
+    combinationIdList = []
+    for buttonId in range(1, len(buttonList) + 1):
+        combinationsLengthX = list(combinations([x for x in range(len(buttonList))], buttonId))
+        for combination in combinationsLengthX:
+            combinationIdList.append([int(x) for x in str(combination)[1:-1].split(",") if x != ""])
+    return combinationIdList
 
 def getMinimumButtonPressesForSolution(solution, buttons):
-    combinations = getAllCombinations(buttons)
-    print(combinations)
-    for combination in combinations:
-        print(combination)
+    combinations = getCombinationIds(buttons)
+    for combinationId in combinations:
+        combination = [buttons[id] for id in combinationId]
         combinationSolution = calculatCombination(combination)
         if combinationIsGood(solution, combinationSolution):
+            print(combinationId)
             return len(combination)
     return len(solution)
 
@@ -72,8 +74,9 @@ def runPartOne(data):
     totalPresses = 0
     for i in data:
         solution, buttons = translateInputStringToLPInput(i)[:2]
+        print(getMinimumButtonPressesForSolution(solution, buttons))
         totalPresses += getMinimumButtonPressesForSolution(solution, buttons)
 
     return totalPresses
 
-runPartOne(test)
+print(runPartOne(test))
