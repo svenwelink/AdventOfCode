932f3f768b6910a757a49f9bce456456ac2740a9
Sven Welink <78083196+svenwelink@users.noreply.github.com>
2025-12-11 14:45:32 +0100

    Refactor runPartOne and add runPartTwo function
    Still needs some work

--- 2025/day11.py
+++ 2025/day11.py
@@ -15,19 +15,33 @@ def prepData(data):
     objectList = [toSplitPoint(id, splitedList[id]) for id in range(len(splitedList))]
     return objectList
 
-def runPartOne(data):
+def runPartOne(data, inString = "you", outString = "out"):
     toSplitPoint = prepData(data)
-    pointsToCheck, totalPaths = ["you"], 1
+    pointsToCheck, totalPaths = [inString], 1
+    print(pointsToCheck)
     while len(pointsToCheck) > 0:
         pointsAfterSplits = []
         for pointName in pointsToCheck:
             point = [x for x in toSplitPoint if x.name == pointName][0]
             totalPaths += len(point.newPoints) - 1
             for x in point.newPoints:
-                if x != "out":
+                if x != outString and x != "out" and x != inString:
                     pointsAfterSplits.append(x) 
 
         pointsToCheck = pointsAfterSplits
     return totalPaths
 
 print(runPartOne(input))
+
+def runPartTwo(data):
+    dacToOut = runPartOne(data, "dac", "out")
+    fftToOut = runPartOne(data, "fft", "out")
+    dacToFft = runPartOne(data, "dac", "fft")
+    fftToDac = runPartOne(data, "fft", "dac")
+    svrToDac = runPartOne(data, "svr", "dac")
+    svrToFft = runPartOne(data, "svr", "fft")
+
+    svrToOut = svrToFft * fftToDac * dacToOut + svrToDac * dacToFft * fftToOut
+    return svrToOut
+
+print(runPartTwo(test))
