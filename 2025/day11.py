530c3b18556d0ffe0d0ba0c3da6429b93979e27a
Sven Welink <78083196+svenwelink@users.noreply.github.com>
2025-12-11 10:49:04 +0100

    Part 1

---
+++ 2025/day11.py
@@ -0,0 +1,38 @@
+import utils
+
+test = [x.strip() for x in utils.importData("TestInput/day11.txt")]
+input = [x.strip() for x in utils.importData("Input/day11.txt")]
+
+class toSplitPoint:
+    def __init__(self, id, values):
+        self.id = int(id)
+        self.name = values[0]
+        self.newPoints = values[1].strip().split(" ")
+        self.splitIds = []
+
+class splitPoint:
+    def __init__(self, values):
+        self.index = values[0]
+        self.paths = values[1]
+
+def prepData(data):
+    splitedList = [x.split(":") for x in data]
+    objectList = [toSplitPoint(id, splitedList[id]) for id in range(len(splitedList))]
+    return objectList
+
+def runPartOne(data):
+    toSplitPoint = prepData(data)
+    pointsToCheck, totalPaths = ["you"], 1
+    while len(pointsToCheck) > 0:
+        pointsAfterSplits = []
+        for pointName in pointsToCheck:
+            point = [x for x in toSplitPoint if x.name == pointName][0]
+            totalPaths += len(point.newPoints) - 1
+            for x in point.newPoints:
+                if x != "out":
+                    pointsAfterSplits.append(x) 
+
+        pointsToCheck = pointsAfterSplits
+    return totalPaths
+
+print(runPartOne(input))
