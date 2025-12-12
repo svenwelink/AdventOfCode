73eb6773ccc7e4901cf4572124f0ca7e4aa999d5
Sven Welink <78083196+svenwelink@users.noreply.github.com>
2025-12-12 15:19:47 +0100

    Refactor path calculations and remove print statements
    Refactor path calculation logic and clean up unused code.

--- 2025/day11.py
+++ 2025/day11.py
@@ -18,14 +18,13 @@ def prepData(data):
 def runPartOne(data, inString = "you", outString = "out"):
     toSplitPoint = prepData(data)
     pointsToCheck, totalPaths = [inString], 1
-    print(pointsToCheck)
     while len(pointsToCheck) > 0:
         pointsAfterSplits = []
         for pointName in pointsToCheck:
             point = [x for x in toSplitPoint if x.name == pointName][0]
             totalPaths += len(point.newPoints) - 1
             for x in point.newPoints:
-                if x != outString and x != "out" and x != inString:
+                if x != outString:
                     pointsAfterSplits.append(x) 
 
         pointsToCheck = pointsAfterSplits
@@ -33,15 +32,56 @@ def runPartOne(data, inString = "you", outString = "out"):
 
 print(runPartOne(input))
 
-def runPartTwo(data):
-    dacToOut = runPartOne(data, "dac", "out")
-    fftToOut = runPartOne(data, "fft", "out")
-    dacToFft = runPartOne(data, "dac", "fft")
-    fftToDac = runPartOne(data, "fft", "dac")
-    svrToDac = runPartOne(data, "svr", "dac")
-    svrToFft = runPartOne(data, "svr", "fft")
+class path:
+    def __init__(self, pathList):
+        self.list = pathList
 
-    svrToOut = svrToFft * fftToDac * dacToOut + svrToDac * dacToFft * fftToOut
+    def lastNode(self):
+        return self.list[-1]
+    
+    def addPoint(self, point):
+        self.list.append(point)
+
+    def containsNoLoop(self):
+        if len(self.list) == len(list(set(self.list))):
+            return True
+        return False
+
+
+def calculatePathsBetweenTwoPoints(data, inString = "you", outString = "out"):
+    pathsToCheck, totalPaths, roundCount= [path([inString])], 0, 0
+    while len(pathsToCheck) > 0 and roundCount < len(data):
+        print("Round: ", str(roundCount), " Paths to check: ", str(len(pathsToCheck)))
+        newPaths = []
+
+        for pathToCheck in pathsToCheck:
+            newPoints = [x.newPoints for x in data if x.name == pathToCheck.lastNode()][0]
+            
+            for point in newPoints:
+                newPath = path(pathToCheck.list.copy())
+                
+                if point != outString and point != "out":
+                    newPath.addPoint(point)
+                    if newPath.containsNoLoop():
+                        newPaths.append(newPath)
+                elif point == outString:
+                    totalPaths += 1
+
+        pathsToCheck = newPaths
+        roundCount += 1
+    return totalPaths
+
+def runPartTwo(data):
+    toSplitPoint = prepData(data)
+
+    dacToOut = calculatePathsBetweenTwoPoints(toSplitPoint, "dac", "out")
+    #fftToOut = calculatePathsBetweenTwoPoints(toSplitPoint, "fft", "out")
+    #dacToFft = calculatePathsBetweenTwoPoints(toSplitPoint, "dac", "fft")
+    fftToDac = calculatePathsBetweenTwoPoints(toSplitPoint, "fft", "dac")
+    #svrToDac = calculatePathsBetweenTwoPoints(toSplitPoint, "svr", "dac")
+    svrToFft = calculatePathsBetweenTwoPoints(toSplitPoint, "svr", "fft")
+
+    svrToOut = svrToFft * fftToDac * dacToOut #+ svrToDac * dacToFft * fftToOut
     return svrToOut
 
-print(runPartTwo(test))
+print(runPartTwo(input))
