2bca0878cf1714841158f1883f1c043fefebba8b
Sven Welink <78083196+svenwelink@users.noreply.github.com>
2025-12-13 11:08:04 +0100

    added a step to pick the startpoints smarter
    Runs a lot faster now

--- 2025/day09.py
+++ 2025/day09.py
@@ -1,4 +1,6 @@
 import utils
+import numpy as np
+import matplotlib.path as mplPath
 from operator import attrgetter
 
 test = utils.importData("TestInput/day09.txt")
@@ -9,6 +11,10 @@ class coords:
         splitString = string.split(",")
         self.x = int(splitString[0])
         self.y = int(splitString[1])
+        self.sizePreviousAndNextPoint = 0
+
+    def setSize(self, size):
+        self.sizePreviousAndNextPoint = size
 
 def calcSquareSize(p1:coords, p2:coords):
     lenght = abs(p1.x - p2.x) + 1
@@ -28,8 +34,19 @@ def runPartOne(data):
 
 print(runPartOne(input))
 
-import matplotlib.path as mplPath
-import numpy as np
+def getStartPoints(allCoords):
+    for i in range(len(allCoords)):
+        if i == 0:
+            allCoords[i].setSize(calcSquareSize(allCoords[-1], allCoords[1]))
+        elif i == (len(allCoords) - 1):
+            allCoords[i].setSize(calcSquareSize(allCoords[i - 1] , allCoords[0]))
+        else:
+            allCoords[i].setSize(calcSquareSize(allCoords[i - 1], allCoords[i + 1]))
+
+    startPointList = allCoords.copy()
+    startPointList = sorted(startPointList, key=attrgetter('sizePreviousAndNextPoint'), reverse=True)
+ 
+    return startPointList[0:2]
 
 def prepDataForPolygon(data):
     newData = []
@@ -64,15 +81,15 @@ def allPointsAreInPolygon(sides, polygon):
 def runPartTwo(data):
     polygon = mplPath.Path(np.array(prepDataForPolygon(data)))
     allCoords = [coords(row) for row in data]
+    startPointList =  getStartPoints(allCoords)
     highestValue = 0
 
-    for x in range(len(allCoords)):
-        print(x)
-        for y in range(x+1, len(allCoords)):
-            size = calcSquareSize(allCoords[x], allCoords[y])
+    for x in range(len(startPointList)):
+        for y in range(0, len(allCoords)):
+            size = calcSquareSize(startPointList[x], allCoords[y])
             if size > highestValue:
-                if squareCornersIsAllowed(allCoords[x], allCoords[y], polygon):
-                    if squareIsAllowed(allCoords[x], allCoords[y], polygon):
+                if squareCornersIsAllowed(startPointList[x], allCoords[y], polygon):
+                    if squareIsAllowed(startPointList[x], allCoords[y], polygon):
                         highestValue = size
     return highestValue
 
